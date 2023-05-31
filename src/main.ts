bf8d29c0fc3f0372ef185a55824d25a730d28203
zjtt <893126716@qq.com>
2023-05-31 19:27:41 +0800

    feat: pinia持久化

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,24 @@ import App from "./App.vue"
 // main.js，注意要在use方法之后执行
 import tmui from "@/tmui"
 import * as Pinia from "pinia"
+import { createPersistedState } from "pinia-plugin-persistedstate"
 
 // #ifdef VUE3
 import { createSSRApp } from "vue"
 export function createApp() {
   const app = createSSRApp(App)
-  const pinia = Pinia.createPinia()
+  const pinia = Pinia.createPinia().use(
+    createPersistedState({
+      storage: {
+        getItem(key: string): string | null {
+          return uni.getStorageSync(key)
+        },
+        setItem(key: string, value: string) {
+          uni.setStorageSync(key, value)
+        }
+      }
+    })
+  )
   app.use(pinia)
   app.use(tmui, { autoDark: false } as Tmui.tmuiConfig)
   // 如此配置即可
